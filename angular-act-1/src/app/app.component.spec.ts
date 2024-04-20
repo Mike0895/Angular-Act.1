3bdc872965dd1a8eb969c26553e69ed9cf0624c3
Mike0895 <michael.morocho05@gmail.com>
2024-04-20 22:40:23 +0200

    Cargar librería de bootstrap

--- angular-act-1/src/app/app.component.spec.ts
+++ angular-act-1/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-app-angular' title`, () => {
+  it(`should have the 'angular-act-1' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app-angular');
+    expect(app.title).toEqual('angular-act-1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-act-1');
   });
 });
--- angular-act-1/src/app/app.component.ts
+++ angular-act-1/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app-angular';
+  title = 'angular-act-1';
 }
