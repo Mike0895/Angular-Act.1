651b57b12e61e0915511bf4e2262839f9d174777
Mike0895 <michael.morocho05@gmail.com>
2024-04-27 16:49:02 +0200

    poner el header

--- angular-act-1/src/app/app.component.ts
+++ angular-act-1/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatMenuModule,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-act-1/src/app/components/inicio/login.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector:"my-componente",
+    template: `
+    <h1>Bienvenidos todos</h1>
+    <p>Estamos creando un componente</p>
+    `
+
+})
+
+export class MyComponente{
+    constructor(){
+        console.log("Se ha cargado el componente");
+    }
+}
